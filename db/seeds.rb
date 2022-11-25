760654da2d1540c6052f8c46c8944af4f6c3429e
ALEH <olegrez@tut.by>
2022-11-25 22:04:02 +0100

    lesson_4. seeds.rb - class DataCreate, Gemfile - <gem 'faker', '~> 2'>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,78 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+class DataCreate
+  def initialize
+    @index = nil
+  end
+
+  def create_users
+    3.times do
+      User.create([name: Faker::Internet.username(specifier: 5..6),
+                   e_mail: Faker::Internet.free_email,
+                   password: Faker::Internet.password(min_length: 5, max_length: 7)])
+    end
+  end
+
+  def create_user_tests
+    User.all.each do |user|
+      2.times do
+        UserTest.create user_id: user.id, test_id: Test.all.sample.id
+      end
+    end
+  end
+
+  def categories_title
+    %i[GreekPhilosophers ProgrammingLanguage Literature]
+  end
+
+  def create_categories
+    categories_title.each do |title|
+      @index = categories_title.index(title)
+      category = Category.create title: title
+      create_tests(category.id)
+    end
+  end
+
+  def test_title
+    [Faker::GreekPhilosophers.name, Faker::ProgrammingLanguage.name, Faker::Book.author][@index]
+  end
+
+  def create_tests(id)
+    4.times do
+      test = Test.create title: test_title, category_id: id, level: rand(0..2)
+      create_questions(test.id)
+    end
+  end
+
+  def create_questions(id)
+    3.times do
+      question = Question.create body: Faker::Lorem.question, test_id: id
+      create_answers(question.id)
+    end
+  end
+
+  def answer_body
+    [Faker::GreekPhilosophers.quote,
+     Faker::Quote.famous_last_words,
+     Faker::Quotes::Shakespeare.as_you_like_it_quote][@index]
+  end
+
+  def create_answers(id)
+    answers = []
+    4.times do
+      answers << Answer.create(question_id: id, body: answer_body)
+    end
+    answers.sample.update(correct: true)
+  end
+
+  def delete_all
+    [Answer, Question, UserTest, Test, Category, User].each(&:delete_all)
+  end
+
+  def create_all
+    delete_all
+    %i[create_categories create_users create_user_tests].each { |method| send method }
+  end
+end
+
+DataCreate.new.create_all
