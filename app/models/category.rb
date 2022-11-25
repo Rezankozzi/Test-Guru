6869dfefd77273293594ea3315df57242b21ef76
ALEH <olegrez@tut.by>
2022-11-25 22:49:50 +0100

    all models: add validations and scopes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@
 
 class Category < ApplicationRecord
   has_many :tests
+
+  default_scope { order(title: :asc) }
+
+  validates :title, presence: { message: "title category can't be blank" }
+  validates :title, uniqueness: { message: 'category with this title already exists' }
 end
